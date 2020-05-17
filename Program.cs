da4604f06794d2f8d79e83ff94d8371404e8e0bd
Jada <mccoyjada.9816@gmail.com>
2020-05-17 14:45:27 -0500

    Chp 3 Exercise 2 GPA Console Program

--- Program.cs
+++ Program.cs
@@ -10,6 +10,15 @@ namespace Exercise2
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter your GPA: ");
+            double gpa = double.Parse(Console.ReadLine());
+
+            if(gpa >= 3.5)
+                Console.WriteLine("Congratulations, you made honor roll!");
+            else if(gpa < 2.0)
+                Console.WriteLine("Sorry, you are now on probation...");
+            else
+                Console.WriteLine("Great job, your grades are in good standing");
         }
     }
 }
